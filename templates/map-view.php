26036291eab3bf4779b20587f718e53e92c79110
Daryl Lozupone <daryl@clubdeuce.net>
2017-12-11 22:28:53 -0500

    Output should be escaped

--- templates/map-view.php
+++ templates/map-view.php
@@ -7,7 +7,7 @@ use Clubdeuce\WPGoogleMaps\Map_View;
  */
 $model = $this->_model;
 ?>
-<div id="<?php echo $model->html_id(); ?>" class="wp-google-map"></div>
+<div id="<?php echo esc_attr( $model->html_id() ); ?>" class="wp-google-map"></div>
 <script type="application/javascript">
 	jQuery(document).ready(function() {
 		generate_map(
