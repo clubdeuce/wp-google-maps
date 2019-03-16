749d6a01bdbda72b4e4499d3d28c2bc4b0f03c74
Daryl Lozupone <daryl@clubdeuce.net>
2019-03-15 22:54:10 -0400

    Use prototypical inheritance in Geocoder class

--- src/modules/Geocoder.js
+++ src/modules/Geocoder.js
@@ -1,22 +1,24 @@
-export default class Geocoder {
+class Geocoder {
     constructor() {
-        this.geocoder = new google.maps.Geocoder;
-    }
-
-    /**
-     *
-     * @param lat
-     * @param lng
-     */
-    addressFromLocation(lat, lng) {
-        let latLng   = new google.maps.LatLng(lat, lng);
-        
-        this.geocoder.geocode({"latLng": latLng}, function (results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-                if (results[0]) {
-                    return results[0].formatted_address;
-                }
-            }
-        });
+        this.geocoder = new google.maps.Geocoder();
     }
 }
+
+/**
+ *
+ * @param lat
+ * @param lng
+ */
+Geocoder.prototype.addressFromLocation = (lat, lng) => {
+    let latLng   = new google.maps.LatLng(lat, lng);
+    
+    this.geocoder.geocode({"latLng": latLng}, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+                return results[0].formatted_address;
+            }
+        }
+    });
+};
+
+export default Geocoder;
