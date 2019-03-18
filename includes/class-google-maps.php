51caec0f4b5bd9d2dbf878ff74d40623a621ee62
Daryl Lozupone <daryl@clubdeuce.net>
2019-03-18 14:05:19 -0400

    Enqueue script in footer

--- includes/class-google-maps.php
+++ includes/class-google-maps.php
@@ -231,7 +231,7 @@ class Google_Maps {
 		$source = sprintf( '%1$s/dist/main.js', self::source_url() );
 
 		wp_register_script( 'google-maps', "https://maps.google.com/maps/api/js?v=3&key={$key}", false, '3.0', true );
-		wp_register_script( 'map-control', $source, array( 'jquery', 'google-maps' ), self::version(), false );
+		wp_register_script( 'map-control', $source, array( 'jquery', 'google-maps' ), self::version(), true );
 		wp_register_style( 'map', self::source_url() . '/assets/map.css' );
 		
 	}
