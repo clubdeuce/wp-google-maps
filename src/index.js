7941ee0765ec64a1486f18a3c262c4eabe7ce978
Daryl Lozupone <daryl@clubdeuce.net>
2022-07-20 01:24:13 -0400

    Implement Qodana code  analysis

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Map from './modules/Map';
 
 window.gmMaps = {};
 
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function () {
 
     if ("https:" === window.location.protocol) {
         userLocation().then(position => {
@@ -23,11 +23,12 @@ jQuery(document).ready(function ($) {
  * Create a Google map and add it to the gmMaps global varable.
  * 
  * @param {string} mapId       The DOM element id that should contain the map
- * @param {oject} mapParams    The map parameters. At least the 'center' element must be defined. See https://developers.google.com/maps/documentation/javascript/reference/map#MapOptions
+ * @param {object} mapParams   The map parameters. At least the 'center' element must be defined. See https://developers.google.com/maps/documentation/javascript/reference/map#MapOptions
  * @param {array} markers      An array containing map markers
  * @param {array} infoWindows  An array containing info window objects
+ * @param {object} options     Map options
  */
-window.generate_map = function(mapId, mapParams, markers, infoWindows, options = []) {
+window.generate_map = function(mapId, mapParams, markers, infoWindows, options = {}) {
     let map            = new Map(mapId, mapParams, markers, infoWindows);
     let gMap           = map.gMap;
     let mapMarkers     = map.addMarkers(markers, gMap);
