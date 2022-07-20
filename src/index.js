88f2ac6bbb0269c8a39f5d9a73e03adeefeb7675
Daryl Lozupone <daryl@clubdeuce.net>
2022-07-20 00:00:40 -0400

    Add missing JSDoc param declaration

--- src/index.js
+++ src/index.js
@@ -23,11 +23,12 @@ jQuery(document).ready(function ($) {
  * Create a Google map and add it to the gmMaps global varable.
  * 
  * @param {string} mapId       The DOM element id that should contain the map
- * @param {object} mapParams    The map parameters. At least the 'center' element must be defined. See https://developers.google.com/maps/documentation/javascript/reference/map#MapOptions
+ * @param {object} mapParams   The map parameters. At least the 'center' element must be defined. See https://developers.google.com/maps/documentation/javascript/reference/map#MapOptions
  * @param {array} markers      An array containing map markers
  * @param {array} infoWindows  An array containing info window objects
+ * @param {object} options     Map options
  */
-window.generate_map = function(mapId, mapParams, markers, infoWindows, options = []) {
+window.generate_map = function(mapId, mapParams, markers, infoWindows, options = {}) {
     let map            = new Map(mapId, mapParams, markers, infoWindows);
     let gMap           = map.gMap;
     let mapMarkers     = map.addMarkers(markers, gMap);
